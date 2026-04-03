c0a72baa4751595d8a690b6349d56e65db655ad4
Jeffrey <color.dove@gmail.com>
2026-04-03 10:17:37 +0800

    fix: fix the migrate error

--- apps/web/lib/database/pglite/migrations/0006_aberrant_texas_twister.sql
+++ apps/web/lib/database/pglite/migrations/0006_aberrant_texas_twister.sql
@@ -1,3 +1,3 @@
 ALTER TABLE "connections" ALTER COLUMN "host" DROP NOT NULL;--> statement-breakpoint
 ALTER TABLE "connections" ALTER COLUMN "port" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "connections" ADD COLUMN "path" text;
+ALTER TABLE "connections" ADD COLUMN IF NOT EXISTS "path" text;
